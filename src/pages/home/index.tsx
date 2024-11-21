9bd99ee2cf2b4409f330f795272cab8a2df1647e
kosssshhhh <kosukwhon@gmail.com>
2024-11-21 18:05:58 +0900

    ✨ Feat: home UI 구성

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,18 +1,21 @@
 import PageContainer from '@components/PageContainer';
-import SearchBar from '@pages/dictionary/_components/SearchBar';
-import { Outlet } from 'react-router-dom';
-
+// import SearchBar from '@pages/dictionary/_components/SearchBar'; // 필요 시 활성화
 import LoadmapCard from './_components/LoadmapCard';
+import WelfareCard from './_components/WelfareCard';
 
 export default function Home() {
 	return (
 		<PageContainer>
-			{/* <SearchBar /> */}
-			<div className="flex flex-col w-full h-full items-center mt-12">
-				<LoadmapCard />
-			</div>
+			<div className="flex flex-col w-full h-full items-center mt-12 gap-12">
+				{/* 검색바 추가 가능 */}
+				{/* <SearchBar /> */}
 
-			{/* <Outlet /> */}
+				{/* 로드맵 카드 */}
+				<LoadmapCard />
+
+				{/* 복지 카드 */}
+				<WelfareCard />
+			</div>
 		</PageContainer>
 	);
 }
