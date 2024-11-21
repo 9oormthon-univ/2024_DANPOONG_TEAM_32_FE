f76d378a8bf40d66679026447b648d5ac6ebb2bb
kosssshhhh <kosukwhon@gmail.com>
2024-11-22 00:20:54 +0900

    ✨ 페이지 이동 간에 애니메이션 구현

--- src/layout/mainLayout/index.tsx
+++ src/layout/mainLayout/index.tsx
@@ -4,15 +4,55 @@ import Header from '@components/Header';
 import Footer from '@components/Footer';
 import { shouldHideHeaderAndFooter } from '@utils/layoutUtils';
 
+import { AnimatePresence, motion } from 'motion/react';
+import { useEffect, useState } from 'react';
+
 export default function MainLayout() {
 	const location = useLocation();
 	const hideHeaderAndFooter = shouldHideHeaderAndFooter(location.pathname);
 
+	// 특정 경로 감지 (startsWith 사용)
+	const shouldAnimate = !location.pathname.startsWith('/youth-map');
+
+	const [prevPage, setPrevPage] = useState(0);
+	const [currPage, setCurrPage] = useState(0);
+
+	useEffect(() => {
+		// 간단히 `pathname`의 길이를 기준으로 페이지 순서 계산
+		const pageOrder = location.pathname.split('/').length;
+		setPrevPage(currPage);
+		setCurrPage(pageOrder);
+	}, [location.pathname]);
+
+	const AppAni = {
+		init: {
+			x: prevPage < currPage ? '-100%' : '100%', // 이전 페이지와 비교해 방향 결정
+			opacity: 0,
+			transition: { type: 'spring', stiffness: 120, damping: 20 },
+		},
+		animate: {
+			x: 0,
+			opacity: 1,
+			transition: { type: 'spring', stiffness: 120, damping: 20 },
+		},
+	};
+
 	return (
 		<main className="flex flex-col h-screen font-pre">
 			{!hideHeaderAndFooter && <Header />}
-			<div className={`flex-1 overflow-y-auto max-w-[980px] w-full mx-auto ${!hideHeaderAndFooter ? 'pb-[60px]' : ''}`}>
-				<Outlet />
+			<div
+				className={`flex-1 overflow-y-auto max-w-[980px] w-full mx-auto scrollbar-hide ${
+					!hideHeaderAndFooter ? 'pb-[60px]' : ''
+				}`}>
+				{shouldAnimate ? (
+					<AnimatePresence mode="wait">
+						<motion.div key={location.pathname} initial="init" animate="animate" exit="exit" variants={AppAni}>
+							<Outlet />
+						</motion.div>
+					</AnimatePresence>
+				) : (
+					<Outlet />
+				)}
 			</div>
 			{!hideHeaderAndFooter && <Footer />}
 		</main>
--- src/pages/home/_components/HomeComponents.tsx
+++ src/pages/home/_components/HomeComponents.tsx
@@ -3,7 +3,7 @@ import WelfareCard from '@pages/home/_components/WelfareCard';
 
 export default function HomeComponents() {
 	return (
-		<div className="flex flex-col w-full h-full items-center mt-12 gap-12">
+		<div className="flex flex-col w-full h-full items-center mt-12 gap-12 pb-20">
 			{/* 로드맵 카드 */}
 			<LoadmapCard />
 
