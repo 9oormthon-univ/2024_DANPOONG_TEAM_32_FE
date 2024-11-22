dd7835d2dbf631fbfd964b8580ccecb814128cd5
JIUUOO <nninjiuuoo@gmail.com>
2024-11-22 12:35:09 +0900

    🐛 Fix: tailwind-scrollbar-hide import 수정

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+import scrollbarHide from 'tailwind-scrollbar-hide';
+
 export default {
 	content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
 	theme: {
@@ -30,5 +32,5 @@ export default {
 			},
 		},
 	},
-	plugins: [require('tailwind-scrollbar-hide')],
+	plugins: [scrollbarHide],
 };
