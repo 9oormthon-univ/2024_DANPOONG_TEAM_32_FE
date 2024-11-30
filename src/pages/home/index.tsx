209a75afe8d3854e77aaca9ee8251744485e7ba5
JIUUOO <nninjiuuoo@gmail.com>
2024-12-01 04:28:22 +0900

    🎨 Style: svg 변환, roadmap 네임 변경

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,6 +1,6 @@
 import PageContainer from '@components/PageContainer';
 // import SearchBar from '@pages/dictionary/_components/SearchBar'; // 필요 시 활성화
-import LoadmapCard from './_components/LoadmapCard';
+import LoadmapCard from './_components/RoadmapCard';
 import WelfareCard from './_components/WelfareCard';
 import { Outlet } from 'react-router-dom';
 
