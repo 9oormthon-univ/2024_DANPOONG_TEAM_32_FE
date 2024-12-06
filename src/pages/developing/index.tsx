a1d191aa24b40125127d2fee27f1223995537da3
JIUUOO <nninjiuuoo@gmail.com>
2024-12-07 03:35:05 +0900

    🎨 Design: 홈 디자인 수정

--- src/pages/developing/index.tsx
+++ src/pages/developing/index.tsx
@@ -2,12 +2,12 @@ import Icon from '@components/Icon';
 
 export default function Developing() {
 	return (
-		<div className="flex flex-col items-center justify-center h-full bg-white px-4">
-			<div className="flex flex-col items-center justify-center mt-52">
+		<div className="flex flex-col items-center justify-center h-[calc(100vh-100px)] bg-white px-4">
+			<div className="flex flex-col items-center justify-center">
 				<h2 className="text-2xl text-black">기능 개발 중이에요...</h2>
 				<h2 className="text-2xl text-black">기대해 주세요🥰</h2>
 			</div>
-			<Icon name="DevelopingIcon" className="mt-12" />
+			<Icon name="DevelopingIcon" className="" />
 		</div>
 	);
 }
